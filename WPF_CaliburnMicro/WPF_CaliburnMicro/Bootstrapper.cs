6e6e755fc92daf479b6ff0e0145aaa17c383cdeb
mbreckon <matt@breckonworld.com>
2019-07-13 20:31:33 +0100

    Add simplest WPF Caliburn.Micro app

---
+++ WPF_CaliburnMicro/WPF_CaliburnMicro/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+namespace WPF_CaliburnMicro
+{
+   public class Bootstrapper : BootstrapperBase
+   {
+      public Bootstrapper()
+      {
+         Initialize();
+      }
+
+      protected override void OnStartup(
+         object sender,
+         StartupEventArgs e)
+      {
+         DisplayRootViewFor<ShellViewModel>();
+      }
+   }
+}
